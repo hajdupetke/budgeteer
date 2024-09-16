d8a1fec247456f9e056e0fffd15ec6dc8c2bdeaf
Hajdu Péter <hajdupetke@gmail.com>
2024-09-16 17:21:38 +0200

    redirect authenticated users to the dashboard

--- src/app/(landing-page)/layout.tsx
+++ src/app/(landing-page)/layout.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import TopBar from './components/TopBar';
 import Footer from './components/Footer';
 import { Inter } from 'next/font/google';
+import { redirect } from 'next/navigation';
+import { auth } from '@/lib/auth';
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] });
@@ -14,11 +16,14 @@ export const metadata: Metadata = {
     'Take control of your finances with Budgeteer, the all-in-one personal finance tracker. Manage your budget, track expenses, and be mindfull of your finances. Start for free today!',
 };
 
-export default function RootLayout({
+export default async function LandingPageLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const session = await auth();
+  if (session?.user) redirect('/dashboard');
+
   return (
     <html lang="en" className={inter.className}>
       <body className="w-screen relative">
