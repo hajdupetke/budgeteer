d53af8dd4708c6bfd849b2d98638a278246f9394
Hajdu Péter <hajdupetke@gmail.com>
2024-09-30 14:10:24 +0200

    Create settings record if user doesn't have one

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,6 +4,7 @@ import github from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { db } from './db';
 import type { Provider } from 'next-auth/providers';
+import { create } from 'domain';
 
 const providers: Provider[] = [google, github];
 
@@ -27,6 +28,13 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       console.log('user signed in!');
 
       // check if user has settings entry, if not create it
+      if (
+        (await db.settings.findFirst({ where: { userId: user.id } })) == null
+      ) {
+        await db.settings.create({
+          data: { user: { connect: { id: user.id } } },
+        });
+      }
 
       return true;
     },
