76489717f55c4d91935c8d2a5e80f9ac72ff22a0
Hajdu Péter <hajdupetke@gmail.com>
2025-04-19 01:44:14 +0200

    wip: budget charts

---
+++ src/app/(authenticated)/budgets/_components/BudgetCharts.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { TrendingUp } from 'lucide-react';
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from 'recharts';
+
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from '@/components/ui/chart';
+
+const chartConfig = {
+  max: {
+    label: 'Max',
+  },
+  amount: {
+    label: 'Current',
+  },
+} satisfies ChartConfig;
+
+export function BudgetCharts({
+  chartData,
+}: {
+  chartData: { name: string; max: number; amount: number }[];
+}) {
+  return (
+    <Card className="size-full">
+      <CardHeader>
+        <CardTitle>Bar Chart - Stacked + Legend</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <ChartContainer config={chartConfig}>
+          <BarChart
+            barCategoryGap={8}
+            data={chartData}
+            layout="vertical"
+            stackOffset="none"
+            margin={{ top: 10, right: 10, left: 0, bottom: 20 }}
+          >
+            <CartesianGrid horizontal strokeDasharray="3 3" />
+            <XAxis type="number" />
+            <YAxis dataKey="name" type="category" width={90} />
+            <ChartTooltip content={<ChartTooltipContent hideLabel />} />
+
+            <Bar
+              dataKey="amount"
+              stackId="a"
+              fill="var(--color-primary-700)"
+              radius={[0, 0, 0, 0]}
+            />
+            <Bar
+              dataKey="max"
+              stackId="a"
+              fill="var(--color-primary-200)"
+              radius={[0, 0, 0, 0]}
+            />
+          </BarChart>
+        </ChartContainer>
+      </CardContent>
+    </Card>
+  );
+}
--- src/app/(authenticated)/budgets/_components/Budgets.tsx
+++ src/app/(authenticated)/budgets/_components/Budgets.tsx
@@ -3,7 +3,12 @@
 import { BudgetWithCategory, PrismaBudgetWithCategory } from '@/types/budget';
 import BudgetList from './BudgetList';
 import NewBudget from './NewBudget';
-import { getCategories, getBudgets } from '@/lib/actions';
+import {
+  getCategories,
+  getBudgets,
+  getExpensesByCategory,
+} from '@/lib/actions';
+import { BudgetCharts } from './BudgetCharts';
 
 const Budgets = async () => {
   const categories = (
@@ -28,13 +33,45 @@ const Budgets = async () => {
     categoryIds: budget.categories.map((category) => category.id),
   })) as BudgetWithCategory[];
 
+  const date = new Date();
+  date.setMonth(date.getMonth() - 1);
+
+  const expensesByCategory = await getExpensesByCategory(
+    date.toLocaleString(),
+    new Date().toLocaleString()
+  );
+
+  /* 
+    Maps over all of the user's budgets and then finds the corresponding categories expenses' and sums it all up
+  */
+  const budgetWithAmount = budgets.map((budget) => ({
+    name: budget.name,
+    max: budget.max,
+    amount: budget.categoryIds
+      .map((id) => {
+        const categoryExpense = expensesByCategory.find(
+          (cat) => cat.categoryId === id
+        );
+
+        return categoryExpense?._sum.amount
+          ? categoryExpense._sum.amount.toNumber()
+          : 0;
+      })
+      .reduce((acc, curr) => acc + curr, 0),
+  }));
+
+  console.log(budgetWithAmount);
+
   return (
     <div className="w-full">
       <div className="flex items-center justify-between">
         <h2 className="font-bold text-3xl">Budgets</h2>
         <NewBudget categories={categories} />
       </div>
-      <BudgetList budgets={budgets} categories={categories} />
+      <div className="flex flex-wrap lg:flex-nowrap gap-2 lg:gap-4 items-center justify-center flex-row">
+        <BudgetList budgets={budgets} categories={categories} />
+        <BudgetCharts chartData={budgetWithAmount} />
+      </div>
     </div>
   );
 };
