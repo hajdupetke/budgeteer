a25af888304a412a6ae63006d5de3c886f12df83
Hajdu Péter <hajdupetke@gmail.com>
2025-04-19 09:39:27 +0200

    fix: convert listing to use the Card component

--- src/app/(authenticated)/transactions/_components/category/CategoryList.tsx
+++ src/app/(authenticated)/transactions/_components/category/CategoryList.tsx
@@ -6,6 +6,7 @@ import EditCategory from './EditCategory';
 import DeleteCategory from './DeleteCategory';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { Card, CardContent } from '@/components/ui/card';
 
 const CategoryList = ({
   categories,
@@ -18,49 +19,51 @@ const CategoryList = ({
 
   return (
     <>
-      <div className="w-full p-4 bg-gray-50 shadow-gray-300 drop-shadow-md rounded-xl my-5">
-        <div className="flex items-center justify-between py-4 border-b-2 border-gray-100">
-          <p className="font-semibold text-gray-500">Category name</p>
-          <p className="basis-2/6 text-center font-semibold text-gray-500">
-            Actions
-          </p>
-        </div>
-        {categories
-          .filter((elem) => elem.userId != null)
-          .map((category, index) => (
-            <div
-              key={category.id}
-              className={cn(
-                'flex items-center justify-between py-4 gap-2 border-t-2 border-gray-100',
-                { 'border-t-0!': index === 0 }
-              )}
-            >
-              <p className="text-2xl flex items-center gap-4 basis-4/6">
-                {category.icon}{' '}
-                <span className="text-lg font-semibold">{category.name}</span>
-              </p>
-              <Button
-                className="basis-1/6 bg-transparent! shadow-none! text-gray-500 font-bold hover:underline hover:text-gray-900"
-                onClick={() => {
-                  setSelected(category);
-                  setDeleteOpen(true);
-                }}
+      <Card className="mt-4">
+        <CardContent>
+          <div className="flex items-center justify-between py-4 border-b-2 border-gray-100">
+            <p className="font-semibold text-gray-500">Category name</p>
+            <p className="basis-2/6 text-center font-semibold text-gray-500">
+              Actions
+            </p>
+          </div>
+          {categories
+            .filter((elem) => elem.userId != null)
+            .map((category, index) => (
+              <div
+                key={category.id}
+                className={cn(
+                  'flex items-center justify-between py-4 gap-2 border-t-2 border-gray-100',
+                  { 'border-t-0!': index === 0 }
+                )}
               >
-                Delete
-              </Button>
+                <p className="text-2xl flex items-center gap-4 basis-4/6">
+                  {category.icon}{' '}
+                  <span className="text-lg font-semibold">{category.name}</span>
+                </p>
+                <Button
+                  className="basis-1/6 bg-transparent! shadow-none! text-gray-500 font-bold hover:underline hover:text-gray-900"
+                  onClick={() => {
+                    setSelected(category);
+                    setDeleteOpen(true);
+                  }}
+                >
+                  Delete
+                </Button>
 
-              <Button
-                className="basis-1/6 bg-primary-200 text-primary-900 font-bold hover:bg-primary-300 transition-colors rounded-xl"
-                onClick={() => {
-                  setSelected(category);
-                  setEditOpen(true);
-                }}
-              >
-                Edit
-              </Button>
-            </div>
-          ))}
-      </div>
+                <Button
+                  className="basis-1/6 bg-primary-200 text-primary-900 font-bold hover:bg-primary-300 transition-colors rounded-xl"
+                  onClick={() => {
+                    setSelected(category);
+                    setEditOpen(true);
+                  }}
+                >
+                  Edit
+                </Button>
+              </div>
+            ))}
+        </CardContent>
+      </Card>
       {editOpen && (
         <EditCategory
           category={selected}
--- src/app/(authenticated)/transactions/_components/transaction/TransactionList.tsx
+++ src/app/(authenticated)/transactions/_components/transaction/TransactionList.tsx
@@ -3,16 +3,11 @@
 import { TransactionWithCategory } from '@/types/transaction';
 import { useState } from 'react';
 import { Button } from '@/components/ui/button';
-import clsx from 'clsx';
 import EditTransaction from './EditTransaction';
 import { TransactionCategory, TransactionType } from '@prisma/client';
 import DeleteTransaction from './DeleteTransaction';
-import {
-  MoveUpRight,
-  MoveDownRight,
-  TrendingUp,
-  TrendingDown,
-} from 'lucide-react';
+import { TrendingUp, TrendingDown } from 'lucide-react';
+import { Card, CardContent } from '@/components/ui/card';
 
 const TransactionList = ({
   transactions,
@@ -29,69 +24,68 @@ const TransactionList = ({
 
   return (
     <>
-      <div className="w-full p-4 bg-gray-50 shadow-gray-300 drop-shadow-md rounded-xl my-5">
-        <div className="grid grid-cols-4 items-center justify-between py-4 border-b-2 border-gray-100">
-          <p className="font-semibold text-gray-500 text-center">Name</p>
-          <p className="font-semibold text-gray-500 text-center">Amount</p>
-          <p className="font-semibold text-gray-500 text-center">Timestamp</p>
-          <p className="basis-2/6 text-center font-semibold text-gray-500">
-            Actions
-          </p>
-        </div>
-        {transactions.map((transaction, index) => (
-          <div
-            key={transaction.id}
-            className={clsx(
-              'grid grid-cols-4  items-center justify-between py-4 border-t-2 border-gray-100',
-              { 'border-t-0!': index === 0 }
-            )}
-          >
-            <p className="text-2xl flex items-center justify-center gap-4">
-              {transaction.type === TransactionType.INCOME ? (
-                <TrendingUp className="text-success-600" />
-              ) : (
-                <TrendingDown className="text-warning-600" />
-              )}
-              {transaction.category.icon}
-              <span className="text-lg font-semibold flex items-center gap-2">
-                {transaction.name}{' '}
-              </span>
+      <Card className="mt-4">
+        <CardContent>
+          <div className="grid grid-cols-4 items-center justify-between py-4">
+            <p className="font-semibold text-gray-500 text-center">Name</p>
+            <p className="font-semibold text-gray-500 text-center">Amount</p>
+            <p className="font-semibold text-gray-500 text-center">Timestamp</p>
+            <p className="basis-2/6 text-center font-semibold text-gray-500">
+              Actions
             </p>
-            <p className="text-center">{transaction.amount} EUR</p>
-            <p className="text-center">
-              {transaction.timestamp.toLocaleString('en-us', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit',
-                hour: '2-digit',
-                minute: '2-digit',
-                hour12: false,
-              })}
-            </p>
-            <div className="flex items-center justify-center gap-4">
-              <Button
-                className="w-content bg-transparent! shadow-none! text-gray-500 font-bold hover:underline hover:text-gray-900 !p-0"
-                onClick={() => {
-                  setSelected(transaction);
-                  setDeleteOpen(true);
-                }}
-              >
-                Delete
-              </Button>
-
-              <Button
-                className="bg-primary-200 text-primary-900 font-bold hover:bg-primary-300 transition-colors rounded-xl"
-                onClick={() => {
-                  setSelected(transaction);
-                  setEditOpen(true);
-                }}
-              >
-                Edit
-              </Button>
-            </div>
           </div>
-        ))}
-      </div>
+          {transactions.map((transaction, index) => (
+            <div
+              key={transaction.id}
+              className="grid grid-cols-4  items-center justify-between py-4 border-t-2 border-gray-100"
+            >
+              <p className="text-2xl flex items-center justify-center gap-4">
+                {transaction.type === TransactionType.INCOME ? (
+                  <TrendingUp className="text-success-600" />
+                ) : (
+                  <TrendingDown className="text-warning-600" />
+                )}
+                {transaction.category.icon}
+                <span className="text-lg font-semibold flex items-center gap-2">
+                  {transaction.name}{' '}
+                </span>
+              </p>
+              <p className="text-center">{transaction.amount} EUR</p>
+              <p className="text-center">
+                {transaction.timestamp.toLocaleString('en-us', {
+                  year: 'numeric',
+                  month: '2-digit',
+                  day: '2-digit',
+                  hour: '2-digit',
+                  minute: '2-digit',
+                  hour12: false,
+                })}
+              </p>
+              <div className="flex items-center justify-center gap-4">
+                <Button
+                  className="w-content bg-transparent! shadow-none! text-gray-500 font-bold hover:underline hover:text-gray-900 !p-0"
+                  onClick={() => {
+                    setSelected(transaction);
+                    setDeleteOpen(true);
+                  }}
+                >
+                  Delete
+                </Button>
+
+                <Button
+                  className="bg-primary-200 text-primary-900 font-bold hover:bg-primary-300 transition-colors rounded-xl"
+                  onClick={() => {
+                    setSelected(transaction);
+                    setEditOpen(true);
+                  }}
+                >
+                  Edit
+                </Button>
+              </div>
+            </div>
+          ))}
+        </CardContent>
+      </Card>
       {editOpen && (
         <EditTransaction
           transaction={selected}
